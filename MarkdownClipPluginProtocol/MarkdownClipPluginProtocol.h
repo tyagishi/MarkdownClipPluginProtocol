163258a0727ca69d42656c18b34e0ed42bd41da5
tyagishi <tyagishi@gmail.com>
2025-03-09 10:17:07 +0900

    Initial Commit

---
+++ MarkdownClipPluginProtocol/MarkdownClipPluginProtocol.h
@@ -0,0 +1,18 @@
+//
+//  MarkdownClipPluginProtocol.h
+//  MarkdownClipPluginProtocol
+//
+//  Created by Tomoaki Yagishita on 2025/03/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarkdownClipPluginProtocol.
+FOUNDATION_EXPORT double MarkdownClipPluginProtocolVersionNumber;
+
+//! Project version string for MarkdownClipPluginProtocol.
+FOUNDATION_EXPORT const unsigned char MarkdownClipPluginProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkdownClipPluginProtocol/PublicHeader.h>
+
+
---
+++ MarkdownClipPluginProtocolTests/MarkdownClipPluginProtocolTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownClipPluginProtocolTests.swift
+//  MarkdownClipPluginProtocolTests
+//
+//  Created by Tomoaki Yagishita on 2025/03/09.
+//
+
+import Testing
+@testable import MarkdownClipPluginProtocol
+
+struct MarkdownClipPluginProtocolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
